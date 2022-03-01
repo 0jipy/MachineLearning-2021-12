451466d3411b2b6e1e09733fe4562f9208ff5b5c
0jipy <96100083+0jipy@users.noreply.github.com>
2022-03-02 01:48:23 +0900

    sugo

--- 91.크롤링/playnomore/playnomore/items.py
+++ 91.크롤링/playnomore/playnomore/items.py
@@ -1,12 +1,7 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
-
 class PlaynomoreItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    title = scrapy.Field()
+    price = scrapy.Field()
+    img = scrapy.Field()
+    link = scrapy.Field()
---
+++ 91.크롤링/run.sh
@@ -0,0 +1,2 @@
+cd playnomore
+scrapy crawl Playnomore -o playnomore2.csv -a category1=apparel -a category2=26
